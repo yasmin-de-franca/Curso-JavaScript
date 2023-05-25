70e1fc19439558c866f12ae5405ae3e5dc3ac7d7
yasmin-de-franca <yasmindefranca1301@gmail.com>
2023-05-24 23:52:36 -0300

    update in aula11 and aula12

---
+++ aula12-módulo D/ex012.js
@@ -0,0 +1,10 @@
+//var agora = new Date()
+var hora = 14
+console.log(`Agora é ${hora} horas`)
+if (hora < 12) {
+    console.log('Bom dia')
+} else if (hora <=18) {
+    console.log('Boa tarde')
+} else {
+    console.log('Boa noite')
+}
