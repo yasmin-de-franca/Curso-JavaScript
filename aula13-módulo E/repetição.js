f99f1b2c24fa20d8b191f56c3192f1a42c8d6cba
yasmin-de-franca <yasmindefranca1301@gmail.com>
2023-06-15 20:49:46 -0300

    Create repetição.js

---
+++ aula13-módulo E/repetição.js
@@ -0,0 +1,40 @@
+//Laço de repetição é quando uma condição é verdadeira. Enquanto a condição for verdadeira, ela volta para ser testada novamente, quantas vezes forem necessessárias.
+
+/*while (condição) {
+    //bloco
+}
+
+function comerPizza(){
+    comerFatia()
+    comerFatia()
+    comerFatia()
+    comerFatia()
+    comerFatia()
+    comerFatia()
+    comerFatia()
+    comerFatia()
+}
+
+function comerPizza(){
+    while (temFatia()){
+        comerFatia()
+    }
+}
+
+do {
+
+}while(condição)
+
+*/
+
+/*var c = 1
+while (c <= 3) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 3)
