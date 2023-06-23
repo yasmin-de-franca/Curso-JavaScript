451f53ee97e10341b498bc2724bacf9b481a0b5b
yasmin-de-franca <yasmindefranca1301@gmail.com>
2023-06-22 22:52:41 -0300

    Create repeticao2.js

---
+++ aula14-módulo E/repeticao2.js
@@ -0,0 +1,15 @@
+//for (inicio;teste;incr) {
+ //bloco}
+
+/*var c=1
+while (c<=10){
+    //bloco
+    c++
+}*/
+
+//Repetição hexagonal*
+
+for (var c=1;c<=10;c++){
+    console.log(c)
+}
+console.log('FIM!')
